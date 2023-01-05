16fd10a3c288436a013a5deefd6d1d8976d0a44d
Ioannis Loudaros <johnloudaros@hotmail.com>
2023-01-05 21:28:52 +0200

    Setting up part 1

---
+++ Set 1/Code/LloydMax.m
@@ -0,0 +1,10 @@
+function [x_q, centers, D] =LloydMax(x, N, min_value, max_value)
+%% Author: Ioannis Loudaros, ΑΜ:1067400
+% x : Σήμα εισόδου υπό την μορφή διανύσματος
+% N : Ο αριθμός των bits/sample που θα χρησιμοποιηθούν
+% max_value : Η μέγιστη αποδεκτή τιμή του σήματος εισόδου
+% min_value : Η ελάχιστη αποδεκτή τιμή του σήματος εισόδου
+
+
+end
+
